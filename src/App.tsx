ba1cc0d37cc9a439bacdc1eb0180a6944feb2a05
ribeiro240 <filipepaiva240@gmail.com>
2021-04-13 22:51:41 -0300

    Start styling buttons

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,39 @@ import GlobalStyles from './styles/globalStyles';
 function App() {
   return (
     <>
-    <main>
-      To Do List
-      <GlobalStyles />
-    </main>
+      <main>
+        <h1>To Do List</h1>
+        <h2>What needs to be done?</h2>
+        <input className='new-task-input'></input>
+        <button className='add-task-button'>Add</button>
+        <div className='tasks-buttons-container'>
+          <button>Show all tasks</button>
+          <button>Show active tasks</button>
+          <button>Show completed tasks</button>
+        </div>
+        <h2>3 tasks remaining</h2>
+        <ul>
+          <li>
+            <input type='checkbox'></input>
+            Eat
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+          <li>
+            <input type='checkbox'></input>
+            Sleep
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+          <li>
+            <input type='checkbox'></input>
+            Repeat
+            <button>Edit</button>
+            <button>Delete</button>
+          </li>
+        </ul>
+        <GlobalStyles />
+      </main>
     </>
   );
 }
--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -28,6 +28,46 @@ export default createGlobalStyle`
     max-width: 1120px;
     height: 90%;
     padding: 20px;
+
+    h1 {
+      font-size: 4rem;
+      margin-bottom: 4rem;
+    }
+
+    h2 {
+      font-size: 2rem;
+    }
+
+    button {
+      cursor: pointer;
+      padding: 7px;
+      border-radius: 6px;
+      border: none;
+      color: white;
+      background-color: #007bff;
+    }
+
+    .new-task-input {
+      margin-right: 10px;
+      border-radius: 6px;
+      border-style: groove;
+      padding: 5px;
+    }
+
+    .add-task-button {
+      border: none;
+
+    }
+
+    .tasks-buttons-container {
+      button + button {
+        margin-left: 10px;
+      }
+
+      &:nth-child(1) {
+        background-color: #17a2b8;
+      }
+    }
   }
 
   @media (max-width: 1120px) {
